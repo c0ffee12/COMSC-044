455e1ffdbbd591b585eacd4636f4f4e188abe9af
Dominic <dominiclopez391@gmail.com>
2019-02-04 20:48:57 -0800

    lab02

---
+++ COMSC-044/lab02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "char is of size " << sizeof(char) << " bytes." << endl;
+	cout << "short int is of size " << sizeof(short int) << " bytes." << endl;
+	cout << "int is of size " << sizeof(int) << " bytes." << endl;
+	cout << "long int is of size " << sizeof(long int) << " bytes." << endl;
+	cout << "long long int is of size " << sizeof(long long int) << " bytes." << endl;
+	cout << "float is of size " << sizeof(float) << " bytes." << endl;
+	cout << "double is of size " << sizeof(double) << " bytes." << endl;
+	cout << "long double is of size " << sizeof(long double) << " bytes." << endl;
+	cin.get();
+}
